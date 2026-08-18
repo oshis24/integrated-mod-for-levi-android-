7534d885f1feb852908d026bb3bc7bafa26796c9
Oshis24 <oshimatcha@gmail.com>
2026-08-18 19:44:41 +0700

    Create ItemEntity.cpp

---
+++ src/minecraft/ItemEntity.cpp
@@ -0,0 +1,8 @@
+#include "levi/minecraft/ItemEntity.hpp"
+
+namespace levi::minecraft {
+
+// ItemEntity native accessors will be connected after the
+// ItemEntity layout has been reconstructed for 1.26.44.3.
+
+} // namespace levi::minecraft
