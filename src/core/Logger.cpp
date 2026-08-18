ad9f353467550dae6e93e920c7e5be8a1dcaa341
Oshis24 <oshimatcha@gmail.com>
2026-08-18 23:11:58 +0700

    Update Logger.cpp

--- src/core/Logger.cpp
+++ src/core/Logger.cpp
@@ -1,103 +1,72 @@
-#include "levi/core/Logger.hpp"
+#include "core/Logger.h"
 
 #include <android/log.h>
+#include <cstdarg>
 #include <cstdio>
-
-namespace levi::core {
 
 namespace {
 
-constexpr const char* kTag = "LeviModules";
+constexpr const char* TAG = "LeviIntegrated";
 
-const char* levelName(LogLevel level) noexcept {
-    switch (level) {
-        case LogLevel::Debug:
-            return "DEBUG";
-        case LogLevel::Info:
-            return "INFO";
-        case LogLevel::Warning:
-            return "WARN";
-        case LogLevel::Error:
-            return "ERROR";
-        default:
-            return "UNKNOWN";
-    }
-}
+void vlog(int priority, const char* fmt, va_list args) {
+    char buffer[1024];
 
-android_LogPriority androidPriority(LogLevel level) noexcept {
-    switch (level) {
-        case LogLevel::Debug:
-            return ANDROID_LOG_DEBUG;
-        case LogLevel::Info:
-            return ANDROID_LOG_INFO;
-        case LogLevel::Warning:
-            return ANDROID_LOG_WARN;
-        case LogLevel::Error:
-            return ANDROID_LOG_ERROR;
-        default:
-            return ANDROID_LOG_INFO;
-    }
-}
-
-} // namespace
-
-void Logger::initialize() {
-    __android_log_print(
-        ANDROID_LOG_INFO,
-        kTag,
-        "[INFO] Logger initialized"
-    );
-}
-
-void Logger::debug(const char* format, ...) {
-    va_list args;
-    va_start(args, format);
-    write(LogLevel::Debug, format, args);
-    va_end(args);
-}
-
-void Logger::info(const char* format, ...) {
-    va_list args;
-    va_start(args, format);
-    write(LogLevel::Info, format, args);
-    va_end(args);
-}
-
-void Logger::warning(const char* format, ...) {
-    va_list args;
-    va_start(args, format);
-    write(LogLevel::Warning, format, args);
-    va_end(args);
-}
-
-void Logger::error(const char* format, ...) {
-    va_list args;
-    va_start(args, format);
-    write(LogLevel::Error, format, args);
-    va_end(args);
-}
-
-void Logger::write(
-    LogLevel level,
-    const char* format,
-    va_list args
-) {
-    char message[2048];
-
-    vsnprintf(
-        message,
-        sizeof(message),
-        format,
+    std::vsnprintf(
+        buffer,
+        sizeof(buffer),
+        fmt,
         args
     );
 
     __android_log_print(
-        androidPriority(level),
-        kTag,
-        "[%s] %s",
-        levelName(level),
-        message
+        priority,
+        TAG,
+        "%s",
+        buffer
     );
 }
 
-} // namespace levi::core
+} // namespace
+
+namespace levi::Logger {
+
+void info(const char* fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+
+    vlog(
+        ANDROID_LOG_INFO,
+        fmt,
+        args
+    );
+
+    va_end(args);
+}
+
+void warn(const char* fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+
+    vlog(
+        ANDROID_LOG_WARN,
+        fmt,
+        args
+    );
+
+    va_end(args);
+}
+
+void error(const char* fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+
+    vlog(
+        ANDROID_LOG_ERROR,
+        fmt,
+        args
+    );
+
+    va_end(args);
+}
+
+} // namespace levi::Logger
