18d16062b566999f87432e308c3bfdfbf20dfdd9
Oshis24 <oshimatcha@gmail.com>
2026-08-18 19:42:32 +0700

    Create ClientInstance.cpp

---
+++ src/minecraft/ClientInstance.cpp
@@ -0,0 +1,8 @@
+#include "levi/minecraft/ClientInstance.hpp"
+
+namespace levi::minecraft {
+
+// ClientInstance currently contains only runtime state.
+// Native accessors will be attached after profile resolution.
+
+} // namespace levi::minecraft
