eaf38ee12c1f98d3863cb000fe3d66675a1558c0
Oshis24 <oshimatcha@gmail.com>
2026-08-18 19:42:54 +0700

    Create ItemRenderer.cpp

---
+++ src/minecraft/ItemRenderer.cpp
@@ -0,0 +1,8 @@
+#include "levi/minecraft/ItemRenderer.hpp"
+
+namespace levi::minecraft {
+
+// Native renderer bindings are installed by the module layer.
+// No guessed RVA is used here.
+
+} // namespace levi::minecraft
