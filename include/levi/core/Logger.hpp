b560bb49b2e171f29ee54607705ff6badf583be1
Oshis24 <oshimatcha@gmail.com>
2026-08-18 23:11:12 +0700

    Update Logger.hpp

--- include/levi/core/Logger.hpp
+++ include/levi/core/Logger.hpp
@@ -1,31 +1,9 @@
 #pragma once
 
-#include <cstdarg>
+namespace levi::Logger {
 
-namespace levi::core {
+void info(const char* fmt, ...);
+void warn(const char* fmt, ...);
+void error(const char* fmt, ...);
 
-enum class LogLevel {
-    Debug,
-    Info,
-    Warning,
-    Error
-};
-
-class Logger final {
-public:
-    static void initialize();
-
-    static void debug(const char* format, ...);
-    static void info(const char* format, ...);
-    static void warning(const char* format, ...);
-    static void error(const char* format, ...);
-
-private:
-    static void write(
-        LogLevel level,
-        const char* format,
-        va_list args
-    );
-};
-
-} // namespace levi::core
+} // namespace levi::Logger
