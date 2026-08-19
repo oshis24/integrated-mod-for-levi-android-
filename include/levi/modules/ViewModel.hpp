1ba5ae30c0bdfc30170930007c718ab6a23092b8
Oshis24 <oshimatcha@gmail.com>
2026-08-19 10:30:08 +0700

    Update ViewModel.hpp

--- include/levi/modules/ViewModel.hpp
+++ include/levi/modules/ViewModel.hpp
@@ -1,12 +1,22 @@
 #pragma once
 
 #include "levi/math/Transform.hpp"
+#include "levi/memory/Hook.hpp"
 #include "levi/modules/Module.hpp"
+
+#include <cstdint>
 
 namespace levi::modules {
 
 class ViewModel final : public Module {
 public:
+    using GetFovFn =
+        float(*)(
+            void* self,
+            float value,
+            int enableVariableFov
+        );
+
     ModuleId id() const noexcept override {
         return ModuleId::ViewModel;
     }
@@ -16,18 +26,18 @@ public:
     }
 
     bool initialize() noexcept override;
-
     void shutdown() noexcept override;
-
     void tick(float deltaTime) noexcept override;
 
     bool enable() noexcept override;
-
     void disable() noexcept override;
 
     bool enabled() const noexcept override;
-
     ModuleStatus status() const noexcept override;
+
+    void bindNativeTarget(
+        std::uintptr_t getFov
+    ) noexcept;
 
     const levi::math::Transform&
     transform() const noexcept {
@@ -44,8 +54,48 @@ public:
         transform_.reset();
     }
 
+    float itemFov() const noexcept {
+        return itemFov_;
+    }
+
+    void setItemFov(
+        float value
+    ) noexcept {
+        itemFov_ = value;
+    }
+
+    bool applyThirdPerson() const noexcept {
+        return applyThirdPerson_;
+    }
+
+    void setApplyThirdPerson(
+        bool value
+    ) noexcept {
+        applyThirdPerson_ = value;
+    }
+
 private:
+    static float fovDetour(
+        void* self,
+        float value,
+        int enableVariableFov
+    ) noexcept;
+
+    static void onPerspective(
+        int perspective
+    ) noexcept;
+
+private:
+    inline static ViewModel*
+        active_{nullptr};
+
+    levi::memory::Hook fovHook_{};
+
     bool enabled_{false};
+    bool thirdPerson_{false};
+    bool applyThirdPerson_{false};
+
+    float itemFov_{70.0f};
 
     ModuleStatus status_{
         ModuleStatus::Disabled
